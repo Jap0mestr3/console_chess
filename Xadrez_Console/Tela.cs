4f1de3638503d294ce2e2f394a04fe66b8a67b31
Kaue Atanes <kaueeaaa@gmail.com>
2018-03-25 22:22:26 -0300

    Inicio de testes de movimentacao

--- Xadrez_Console/Tela.cs
+++ Xadrez_Console/Tela.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Tabuleiro;
+using Xadrez;
 
 namespace Xadrez_Console
 {
@@ -26,6 +27,15 @@ namespace Xadrez_Console
             }
             Console.WriteLine("  A B C D E F G H");
         }
+
+        public static PosicaoXadrez lerPosicaoXadrez()
+        {
+            string s = Console.ReadLine();
+            char coluna = s[0];
+            int linha = int.Parse(s[1] + "");
+            return new PosicaoXadrez(coluna, linha);
+        }
+
         public static void imprimirPeca(Peca peca)
         {
             if(peca.cor == Cor.Branca)
